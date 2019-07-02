80327ac8031f33a2f69b7aba5aa85071d1339993
Matas <nilenon@gmail.com>
2019-07-02 18:15:06 +0700

    [Add] Semantic UI & Faker js

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,19 +1,31 @@
 // Import the React and ReactDOM liberies
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from 'faker';
 
 
 
 //Create a React Components
 const App = () => {
-    const Button = {text:'Click Me'};
-  return (
-      <div>
-          <label className ="label" htmlFor="name">Enter name:</label>
-          <input id="name" type="text"/>
-          <button style={{backgroundColor:'blue' , color:'white'}}>{Button.text}</button>
-      </div>
-  );
+
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="avatar">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6.00PM</span>
+                    </div>
+                    <div className="text">Nice blog Posts</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 // Take the react component and show it on the Screen
