f8cb81e808661c03a298c9e80c74478020b695f8
Matas <nilenon@gmail.com>
2019-07-02 19:22:25 +0700

    [Add] html tag to component

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -13,7 +13,10 @@ import Approval from "./Approval";
 const App = () => {
   return (
     <div className="ui container comments">
-      
+      <Approval ><div>
+            <h4>Warning</h4>
+            <p>Are you sure to do this</p>
+          </div></Approval>
       <Approval ><Comment name="Sam" post="Wow!!" date="Today at 6.00PM" avatar={faker.image.avatar()}/></Approval>
       <Approval ><Comment name="Alex" post="I love It" date="Today at 4.25PM" avatar={faker.image.avatar()}/></Approval>
       <Approval ><Comment name="Lisa" post="Haha" date="Yesterday at 3.00PM" avatar={faker.image.avatar()}/></Approval>
