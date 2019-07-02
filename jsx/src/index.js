121e2dac1033633c0d824f1c8e885814f672ce7d
Matas <nilenon@gmail.com>
2019-07-02 18:27:40 +0700

    [Edit] Extract Component

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,31 +1,16 @@
 // Import the React and ReactDOM liberies
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from 'faker';
-
-
+import faker from "faker";
+import Comment from "./Comment";
 
 //Create a React Components
 const App = () => {
-
-    return (
-        <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="avatar">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6.00PM</span>
-                    </div>
-                    <div className="text">Nice blog Posts</div>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="ui container comments">
+      <Comment />
+    </div>
+  );
 };
 
 // Take the react component and show it on the Screen
