af5b4b5bf9e40bc291cccf234dfe7b50367dc3d3
Matas <nilenon@gmail.com>
2019-07-05 10:58:33 +0700

    [Add] ImageList

--- imageapp/src/components/imageList.js
+++ imageapp/src/components/imageList.js
@@ -1,11 +1,22 @@
 import React from "react";
+import "./imageList";
 
 const imageList = props => {
   const images = props.images.map(image => {
-    return <img src={image.urls.small} alt=""/>;
+    return (
+      <div class="column">
+        <img src={image.urls.small} alt="" style={{width:'350px',height:'200px'}}/>
+      </div>
+    );
   });
-  
-  return <div>{images}</div>;
+
+  return (
+    <div>
+      <div class="ui three column grid">
+        {images}
+      </div>
+    </div>
+  );
 };
 
 export default imageList;
