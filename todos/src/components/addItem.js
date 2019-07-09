2a169888e4b046e74313fd802c94068d0e7f7538
Matas <nilenon@gmail.com>
2019-07-09 16:07:36 +0700

    [Add]

--- todos/src/components/addItem.js
+++ todos/src/components/addItem.js
@@ -1,14 +1,28 @@
 import React from "react";
 
 class addItem extends React.Component {
+  state = { input: "" };
+  getInput = e => {
+    this.setState({ input: e.target.value });
+  };
+  onSubmitForm = e => {
+    e.preventDefault();
+    console.log(this.state.input);
+    this.setState({input:''})
+  };
   render() {
     return (
       <div>
-        <input type="text" />
-        <input type="submit"/> 
+        <form onSubmit={this.onSubmitForm}>
+          <input
+            value={this.state.input}
+            onChange={this.getInput}
+            type="text"
+          />
+        </form>
       </div>
     );
   }
 }
 
-export default addItem
+export default addItem;
