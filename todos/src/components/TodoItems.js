f7eab02f974b584e1040a0e91d6619687d45b153
Matas <nilenon@gmail.com>
2019-07-11 12:06:39 +0700

    [Add] Todolist

--- todos/src/components/TodoItems.js
+++ todos/src/components/TodoItems.js
@@ -2,33 +2,30 @@ import React from "react";
 
 class TodoItems extends React.Component {
   changestatus = () => {
-    if(this.props.todo.completed === true){
-        this.props.todo.completed = false;
-    }else{
-        this.props.todo.completed = true
+    if (this.props.todo.completed === true) {
+      this.props.todo.completed = false;
+    } else {
+      this.props.todo.completed = true;
     }
 
     this.props.changecompleted(this.props.todo.completed);
-    
   };
   render() {
-    
-    const style = { color: "red" };
-    const checkCompleted = () => {
-      if (this.props.todo.completed === true) {
-        return style;
-      } else {
-        return null;
-      }
-    };
-
     return (
       <div>
-        <div style={checkCompleted()}>{this.props.todo.title}</div>
+        <div style={this.checkCompleted()}>{this.props.todo.title}</div>
         <button onClick={this.changestatus}>Change</button>
       </div>
     );
   }
+
+  checkCompleted = () => {
+    if (this.props.todo.completed === true) {
+      return {color:"red"};
+    } else {
+      return null;
+    }
+  };
 }
 
 export default TodoItems;
